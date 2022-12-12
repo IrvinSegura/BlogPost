cde0cf13d0ec443a17b727b9077aa0fd05483a86
Irvin_Segura_Ortiz <113122729+IrvinSegura@users.noreply.github.com>
2022-12-12 00:42:40 -0600

    Actualizacion de js

--- publicacion.js
+++ publicacion.js
@@ -1,36 +1,54 @@
+const categoria = document.getElementById('tipo_categoria').value;
+const img = document.getElementById('img').value;
 const output = document.getElementById('app');
+
 class Blog {
-  constructor (body, date, category) {
+  constructor (body, date) {
     this.body = body;
     this.date = date;
   }
   
 }
+
 let blogs = [];
 function returnPost() {
   const postText = document.getElementById("blogText");
   blogs.push(new Blog(postText.value, new Date()));
   output.innerHTML = '';
   let html ='';  
+
+  if(postText.value == '') {
+    alert('No se puede publicar un post vacio');
+    return;
+  }
   
   blogs.sort((a, b) => {
     return new Date(b.date) - new Date(a.date);
   }).forEach((blog) => {
     html += 
     `
-      <p>${blog.body} </br>
-         <b>Publicado:</b> ${blog.date.toLocaleDateString()}, 
-         ${blog.date.toLocaleTimeString()}
-         imagen subida con id file 
-          <img src="https://firebasestorage.googleapis.com/v0/b/your-project-id.appspot.com/o/images%2Ffile?alt=media&token=your-token" alt="file" />
-         <b> Minutos transcurridos:</b> ${Math.round((new Date() - new Date(blog.date)) / 60000)}
-      </p>
-
-      <hr>
+     <center>
+     <table border=2 style="background-color: orangered; width:400px;">
+     <tr>
+       <td>
+       <div style="background-color: yellow;">
+       <b>Categoria:</b> ${categoria} </br>
+       </div>
+       <p>${blog.body} </br>
+        <img src="${img}" width="100" height="100">
+       </p>
+       <div style="background-color: orange;">
+       <b>Publicado:</b> ${blog.date.toLocaleDateString()}<br>
+       <b>Hora: </b>${blog.date.toLocaleTimeString()}
+       </div>
+       </td>
+     </tr>
+   </table>
+    </center>
     `
-    
   });
   output.innerHTML = html;
   postText.value = "";
 }
+
 
