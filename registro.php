5b9973cc59ac98788f99b691865591aee47d625d
Irvin_Segura_Ortiz <113122729+IrvinSegura@users.noreply.github.com>
2022-12-12 00:47:51 -0600

    Actualizacion de registro.php

--- registro.php
+++ registro.php
@@ -16,15 +16,32 @@
 <body>
     <center>
         <br><br><br><br><br><br><br><br><br>
-        <form action="publicacion.php" method="post" style="border:2px solid red; max-width:400px; height: 400px;; background-color: orangered;">
+        <form action="login.php" method="post" style="border:2px solid red; max-width:400px; height: 400px;; background-color: orangered;">
             <h1>Registrarse</h1>
-            Nombre: <input type="text" name="nombre" placeholder="Nombre"><br><br>
-            Apellido: <input type="text" name="apellido" placeholder="Apellido"><br><br>
-            Usuario: <input type="text" name="usuario" placeholder="Usuario"><br><br>
-            Contraseña: <input type="password" name="contraseña" placeholder="Contraseña"><br><br>
+            Tipo de Usuario: <select name="tipo_usuario" id="tipo_usuario">
+                <option value="Admin">Administrador</option>
+                <option value="Usuario">Usuario</option>
+            </select><br><br>
+            Nombre: <input type="text" id="nombre" name="nombre" placeholder="Nombre"><br><br>
+            Apellido: <input type="text"id="apellido" name="apellido" placeholder="Apellido"><br><br>
+            Usuario: <input type="text" id="usuario" name="usuario" placeholder="Usuario"><br><br>
+            Email: <input type="email" id="email" name="email" placeholder="Email"><br><br>
+            Contraseña: <input type="password" id="password" name="contraseña" placeholder="Contraseña"><br><br>
             <input type="submit" value="Registrarse"><br><br>
             Ya tienes cuenta? <a href="login.php">Inicia Sesion</a>
         </form>
     </center>
 </body>
 </html>
+<?php    
+    $tipo_usuario = $_POST['tipo_usuario'];
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $usuario = $_POST['usuario'];
+    $email = $_POST['email'];
+    $contraseña = $_POST['contraseña'];
+    $conexion = mysqli_connect ("localhost", "root", "", "blog");
+    $consulta = "INSERT INTO usuario (tipo_usuario, nombre, apellido, usuario, email, password) VALUES ('$tipo_usuario', '$nombre', '$apellido', '$usuario', '$email', '$contraseña')";
+    $resultado = mysqli_query($conexion, $consulta);
+    mysqli_close($conexion);
+?>
