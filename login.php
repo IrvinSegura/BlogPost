a53e6f882c4ce99a2245a02b92fbff9a0339ec94
Irvin_Segura_Ortiz <113122729+IrvinSegura@users.noreply.github.com>
2022-12-12 00:47:01 -0600

    Actualizacion de login.php

--- login.php
+++ login.php
@@ -27,15 +27,4 @@
 </body>
 </html>
 <?php
-    $usuario = $_POST['usuario'];
-    $contraseña = $_POST['contraseña'];
-    $conexion = mysqli_connect ("localhost ", "root", "", "blog_post");
-    $consulta = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND contraseña = '$contraseña'";
-    $resultado = mysqli_query($conexion, $consulta);
-    $filas = mysqli_num_rows($resultado);
-    if($filas){
-        header("location:publicacion.php");
-    }else{
-        echo "Error en la autenticacion";
-    }
 ?>
